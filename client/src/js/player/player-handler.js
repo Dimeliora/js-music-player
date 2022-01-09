f5fb11c8f192d1e381b974479e581b38fda0178e
Dimeliora <dimeliora710@gmail.com>
2022-01-09 19:52:15 +0900

    autoplay next track till the end of album

--- client/src/js/player/player-handler.js
+++ client/src/js/player/player-handler.js
@@ -1,6 +1,7 @@
 import { ee } from '../helpers/event-emitter';
 import { state } from '../state/state';
 import { playerElms } from '../dom/dom-elements';
+import { getFormattedDuration } from '../helpers/duration-formatter';
 import { getTrackFile } from '../service/fetch-data';
 import {
     updatePlayerAfterAlbumSelection,
@@ -9,26 +10,29 @@ import {
     currentTimeUpdateHandler,
 } from './player-view-updates';
 
-const trackSelectHandler = (e) => {
+const updateSelectedTrack = (trackId) => {
+    state.selectedTrack = state.selectedAlbum.tracklist.find(
+        ({ id }) => id === trackId
+    );
+
+    playerElms.playerAudioElm.pause();
+    playerElms.playerAudioElm.src = getTrackFile(
+        state.selectedAlbum.id,
+        state.selectedTrack.id
+    );
+
+    updatePlayerAfterTrackSelection();
+};
+
+const trackClickHandler = (e) => {
     const trackElm = e.target.closest('[data-track-id]');
     if (!trackElm) {
         return;
     }
 
     const trackId = trackElm.dataset.trackId;
-
     if (state.selectedTrack?.id !== trackId) {
-        state.selectedTrack = state.selectedAlbum.tracklist.find(
-            ({ id }) => id === trackId
-        );
-
-        playerElms.playerAudioElm.pause();
-        playerElms.playerAudioElm.src = getTrackFile(
-            state.selectedAlbum.id,
-            state.selectedTrack.id
-        );
-
-        updatePlayerAfterTrackSelection();
+        updateSelectedTrack(trackId);
     }
 
     if (playerElms.playerAudioElm.paused) {
@@ -40,18 +44,48 @@ const trackSelectHandler = (e) => {
     }
 };
 
+const playerOnTimeUpdatedHandler = () => {
+    currentTimeUpdateHandler();
+    ee.emit('player/time-updated', playerElms.playerAudioElm.currentTime);
+};
+
+const progressClickHandler = (newCurrentTime) => {
+    playerElms.playerAudioElm.currentTime = newCurrentTime;
+};
+
+const playingTrackEndsHandler = () => {
+    const trackElm = playerElms.playerTracklistElm.querySelector(
+        `[data-track-id="${state.selectedTrack.id}"]`
+    );
+    const trackIndex = state.selectedAlbum.tracklist.findIndex(
+        (track) => track.id === state.selectedTrack.id
+    );
+
+    const nextTrack = state.selectedAlbum.tracklist[trackIndex + 1];
+    if (nextTrack !== undefined) {
+        const nextTrackElm = trackElm.nextElementSibling;
+
+        updateSelectedTrack(nextTrackElm.dataset.trackId);
+
+        nextTrackElm.classList.add('track--playing');
+        playerElms.playerAudioElm.play();
+    } else {
+        playerElms.playerAudioElm.currentTime = 0;
+        trackElm.classList.remove('track--playing');
+    }
+};
+
 ee.on('albums/album-selected', updatePlayerAfterAlbumSelection);
 
 playerElms.playerHideElm.addEventListener('click', hidePlayerHandler);
 
-playerElms.playerTracklistElm.addEventListener('click', trackSelectHandler);
+playerElms.playerTracklistElm.addEventListener('click', trackClickHandler);
 
-playerElms.playerAudioElm.addEventListener('timeupdate', () => {
-    currentTimeUpdateHandler();
+playerElms.playerAudioElm.addEventListener(
+    'timeupdate',
+    playerOnTimeUpdatedHandler
+);
 
-    ee.emit('player/time-updated', playerElms.playerAudioElm.currentTime);
-});
+ee.on('player/progress-click', progressClickHandler);
 
-ee.on('player/progress-click', (newCurrentTime) => {
-    playerElms.playerAudioElm.currentTime = newCurrentTime;
-});
+playerElms.playerAudioElm.addEventListener('ended', playingTrackEndsHandler);
