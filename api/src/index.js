6e70c9e65060a26704efc9787626fec13eb29eea
Dimeliora <dimeliora710@gmail.com>
2022-01-10 17:01:46 +0900

    port property name fix

--- api/src/index.js
+++ api/src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const { corsMW } = require('./middlewares/corsMW');
 const { albumsRouter } = require('./routes/albums.routes');
 
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
